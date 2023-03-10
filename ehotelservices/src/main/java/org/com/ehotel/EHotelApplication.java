b8b13c77202569bd294aed9cf224693a3dcc2c5c
Memo-Aldu <maldu064@uOttawa.ca>
2023-03-10 11:16:57 -0500

    config changes

--- ehotelservices/src/main/java/org/com/ehotel/EHotelApplication.java
+++ ehotelservices/src/main/java/org/com/ehotel/EHotelApplication.java
@@ -1,11 +1,9 @@
 package org.com.ehotel;
 
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Data
 public class EHotelApplication {
     public static void main(String[] args) {
         SpringApplication.run(EHotelApplication.class, args);
