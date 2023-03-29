8af3778739d2c8702b091932302135b215405a01
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-03-29 14:55:17 -0400

    Add files via upload

---
+++ ehotelservices/src/main/java/org/com/ehotel/service/hotel/ChainHotelController.java
@@ -0,0 +1,134 @@
+package org.com.ehotel.controller.hotel;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.com.ehotel.dto.hotel.ChainHotelDTO;
+import org.com.ehotel.dto.hotel.HotelDTO;
+import org.com.ehotel.exceptions.BadRequestException;
+import org.com.ehotel.helper.AppHttpResponse;
+import org.com.ehotel.helper.ResponseHandler;
+import org.com.ehotel.service.hotel.ChainHotelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@RestController @AllArgsConstructor
+@RequestMapping("/api/v1/hotel_chain") @Slf4j
+public class ChainHotelController {
+
+    private final ChainHotelService chainService;
+    private final ResponseHandler responseHandler;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<AppHttpResponse> getChainHotelByName(
+            @PathVariable String name, HttpServletRequest request) {
+        if(name == null) {
+            throw new BadRequestException("Invalid name");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "Hotel chain", chainService.getChainHotelEntityByName(name),
+                                "Chain name", name))
+                        .message("Chain found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppHttpResponse> getChainHotelByID(
+            @PathVariable Integer id, HttpServletRequest request) {
+        if(id == null) {
+            throw new BadRequestException("Invalid id");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "hotel chain", chainService.getChainHotelEntityById(id),
+                                "ChainId", id))
+                        .message("Chain found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<AppHttpResponse> updateChainHotel(
+            @PathVariable Integer id, @RequestBody ChainHotelDTO dto,
+            HttpServletRequest request) {
+        if(id == null) {
+            throw new BadRequestException("Hotel id cannot be null");
+        }
+        if(!dto.isValidDto()) {
+            throw new BadRequestException("Invalid extension");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .success(true)
+                        .message("Hotel updated successfully")
+                        .data(Map.of("extension", chainService
+                                .updateChain(dto, id)))
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.OK)
+                        .build(),
+                setupResponseHeaders(request));
+    }
+
+    @PostMapping
+    public ResponseEntity<AppHttpResponse> updateExtension(
+            @RequestBody ChainHotelDTO dto, HttpServletRequest request) {
+        if(!dto.isValidDto()) {
+            throw new BadRequestException("Invalid hotel chain");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .success(true)
+                        .message("Hotel chain updated successfully")
+                        .data(Map.of("hotel chain", chainService
+                                .createChain(dto)))
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.CREATED)
+                        .build(),
+                setupResponseHeaders(request));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AppHttpResponse> deleteChainHotel(
+            @PathVariable Integer id, HttpServletRequest request) {
+        if (id == null) {
+            throw new BadRequestException("Invalid chain id");
+        }
+        chainService.deleteChainHotelEntityByID(id);
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of("id", id))
+                        .message("Chain deleted")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+
+    private HttpHeaders setupResponseHeaders(HttpServletRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(CONTENT_TYPE, "application/json");
+        headers.set(AUTHORIZATION, request.getHeader(AUTHORIZATION));
+        return headers;
+    }
+}
---
+++ ehotelservices/src/main/java/org/com/ehotel/service/hotel/HotelController.java
@@ -0,0 +1,152 @@
+package org.com.ehotel.controller.hotel;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.com.ehotel.dto.hotel.ChainHotelDTO;
+import org.com.ehotel.exceptions.BadRequestException;
+import org.com.ehotel.helper.AppHttpResponse;
+import org.com.ehotel.helper.ResponseHandler;
+import org.com.ehotel.service.hotel.ChainHotelService;
+import org.com.ehotel.service.hotel.HotelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.com.ehotel.dto.room.CommodityDTO;
+import org.com.ehotel.exceptions.BadRequestException;
+import org.com.ehotel.helper.AppHttpResponse;
+import org.com.ehotel.helper.ResponseHandler;
+import org.com.ehotel.service.room.CommodityService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@RestController @AllArgsConstructor
+@RequestMapping("/api/v1/hotel") @Slf4j
+public class HotelController {
+
+    private final HotelService hotelService;
+    private final ResponseHandler responseHandler;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<AppHttpResponse> getHotelByName(
+            @PathVariable String name, HttpServletRequest request) {
+        if(name == null) {
+            throw new BadRequestException("Invalid name");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "Hotel", hotelService.getHotelEntityByName(name),
+                                "Hotel name", name))
+                        .message("Hotel found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppHttpResponse> getHotelByID(
+            @PathVariable Integer id, HttpServletRequest request) {
+        if(id == null) {
+            throw new BadRequestException("Invalid id");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "hotel", hotelService.getHotelEntityById(id),
+                                "HotelId", id))
+                        .message("Hotel found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+    @GetMapping("/{address}")
+    public ResponseEntity<AppHttpResponse> getHotelByAddress(
+            @PathVariable String address, HttpServletRequest request) {
+        if(address == null) {
+            throw new BadRequestException("Invalid address");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "hotel", hotelService.getHotelEntityByAddress(address),
+                                "Hotel address", address))
+                        .message("Hotel found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+    @GetMapping("/{rating}")
+    public ResponseEntity<AppHttpResponse> getAllHotelByRating(
+            @PathVariable short rating, HttpServletRequest request) {
+        if(rating < 0) {
+            throw new BadRequestException("Invalid address");
+        }
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of(
+                                "hotel", hotelService.getAllHotelEntityByRating(rating),
+                                "Hotel rating", rating))
+                        .message("Hotel found")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AppHttpResponse> deleteHotel(
+            @PathVariable Integer id, HttpServletRequest request) {
+        if (id == null) {
+            throw new BadRequestException("Invalid chain id");
+        }
+        hotelService.deleteHotelEntityById(id);
+        return responseHandler.httpResponse(
+                AppHttpResponse.builder()
+                        .data(Map.of("id", id))
+                        .message("Chain deleted")
+                        .status(HttpStatus.OK)
+                        .success(true)
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                setupResponseHeaders(request)
+        );
+    }
+
+
+    private HttpHeaders setupResponseHeaders(HttpServletRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(CONTENT_TYPE, "application/json");
+        headers.set(AUTHORIZATION, request.getHeader(AUTHORIZATION));
+        return headers;
+    }
+}
