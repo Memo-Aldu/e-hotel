3cb466a65329144a94605a432e688ae660dd4e6e
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-03-29 15:09:35 -0400

    Update ChainHotelDTO.java

--- ehotelservices/src/main/java/org/com/ehotel/dto/hotel/ChainHotelDTO.java
+++ ehotelservices/src/main/java/org/com/ehotel/dto/hotel/ChainHotelDTO.java
@@ -1,13 +1,12 @@
 package org.com.ehotel.dto.hotel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
-/**
- * @author : memo-aldu
- * @mailto : maldu064@uOttawa.ca
- * @created : 3/13/2023, Monday
- **/
 public record ChainHotelDTO(
         Integer id, String name, String email, String phoneNumber,
         Short rating, String address) {
+    @JsonIgnore
+    public boolean isValidDto() {
+        return name != null || email != null || phoneNumber != null || rating != null || address != null;
+    }
 }
