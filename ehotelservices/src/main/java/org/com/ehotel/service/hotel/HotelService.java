36016a9b4e354e2cfcf1a5dab4762819d207ea66
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-04-06 16:24:55 -0400

    Update HotelService.java

--- ehotelservices/src/main/java/org/com/ehotel/service/hotel/HotelService.java
+++ ehotelservices/src/main/java/org/com/ehotel/service/hotel/HotelService.java
@@ -1,18 +1,17 @@
 package org.com.ehotel.service.hotel;
 
 import org.com.ehotel.dto.hotel.HotelDTO;
-
 import java.util.Set;
 
-/**
- * @author : memo-aldu
- * @mailto : maldu064@uOttawa.ca
- * @created : 4/4/2023, Tuesday
- **/
 public interface HotelService {
-    HotelDTO getHotelById(Integer id);
-    Set<HotelDTO> getHotelsByChainId(Integer chainId);
+
+    HotelDTO getHotelEntityById(Integer id);
+    Set<HotelDTO> getAllHotelEntities();
+
+    void deleteHotelEntityById(Integer id);
+
     HotelDTO createHotel(HotelDTO hotelDTO);
+
     HotelDTO updateHotel(HotelDTO hotelDTO, Integer id);
-    void deleteHotelById(Integer id);
+
 }
